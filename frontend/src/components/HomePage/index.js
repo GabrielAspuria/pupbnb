3b2db28f09df0e1ffe80b4c058ec2b1e3f4c6a84
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2021-12-12 23:30:56 -0800

    homepage done

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,12 @@
+import './Home.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to pupbnb</h1>
+            <img className="main" src="https://res.cloudinary.com/gabrielaspuria/image/upload/v1639338574/pupbnb/GabooSleep2_drwtkb.png" alt="background" />
+        </div>
+    )
+}
+
+export default HomePage;
