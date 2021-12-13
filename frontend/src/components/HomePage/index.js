1b4542582ce9a4509cbf766f505655ddf1c02068
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2021-12-13 07:14:32 -0800

    added github link

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -5,6 +5,7 @@ const HomePage = () => {
         <div className='homepage'>
             <h1 className='title'>welcome to pupbnb 🐾</h1>
             <img className="pupimage" src="https://res.cloudinary.com/gabrielaspuria/image/upload/v1639338574/pupbnb/GabooSleep2_drwtkb.png" alt="background" />
+            <a id='github' href="https://github.com/GabrielAspuria/pupbnb">Github</a>
         </div>
     )
 }
